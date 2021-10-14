8f5ce20f48ff2e0507e14e19951b941ef91f5d54
dende-h <miyuki_dende@yahoo.co.jp>
2021-10-15 07:27:31 +0900

    例外処理実装途中

--- src/main/java/io/post/novel/controller/NovelContoller.java
+++ src/main/java/io/post/novel/controller/NovelContoller.java
@@ -2,7 +2,6 @@ package io.post.novel.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +10,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.post.novel.auth.UserForm;
 import io.post.novel.dto.NovelRequest;
@@ -23,19 +25,54 @@ import io.post.novel.entity.NovelEntity;
 import io.post.novel.service.NovelService;
 
 @Controller
+@SessionAttributes("session")
 public class NovelContoller {
 	
 	
 	@Autowired NovelService novelService;
 	@Autowired HttpSession session;
 	
+	@ModelAttribute("novelSession")
+	NovelRequest setUpNovelSession()throws HttpSessionRequiredException{
+		System.out.println("create inputForm");
+		return new NovelRequest();
+	}
+	
 	
 	/*
 	 * フォームへ遷移
 	 */
+	
 	@RequestMapping("/novel/write")
-	public String toNovelPage( @ModelAttribute NovelRequest draft, Model model, HttpServletRequest request) {
-		try { 
+	public String toNovelPage(@ModelAttribute("session") NovelRequest draft, Model model){
+		
+		try {
+			setUpNovelSession();
+		} catch (HttpSessionRequiredException e){
+			return "novel/new_novel";
+		}
+		
+		
+		System.out.println(draft);
+		
+		model.addAttribute("draft", draft);
+		
+		return "novel/new_novel";
+		}	
+	
+	public String sessionExceprion(Model model) {
+		return "novel/new_novel";
+	}
+	
+	
+	@RequestMapping("/novel/write/save")
+	public String toNovelPageSave( @ModelAttribute("session") NovelRequest draft, Model model) {
+		
+		
+		System.out.println(draft);
+		
+		model.addAttribute("draft", draft);
+		/*try { 
 			boolean req = session.getAttribute("genre").equals(0);
 			
 			if(req == false){
@@ -54,7 +91,6 @@ public class NovelContoller {
 				model.addAttribute("draft",draft);
 				
 				return "novel/new_novel";
-			} 
 		}
 		//System.out.println(req);
 		//int draftSession = principal.getGenre();
@@ -63,7 +99,8 @@ public class NovelContoller {
 			
 			return "novel/new_novel";
 		}
-
+		*/
+		
 		return "novel/new_novel";
 	}
 	
@@ -99,17 +136,21 @@ public class NovelContoller {
 	
 	
 	@PostMapping(value = "/novel/save/draft",params = "draft_session" )
-	public String setSession(@ModelAttribute @Validated NovelRequest novelSession, BindingResult result,Model model) {
+	public String setSession(@ModelAttribute @Validated NovelRequest novelSession, BindingResult result,Model model,RedirectAttributes redirectAttributes) {
 		 if (result.hasErrors()) {
 			  
 			  return "novel/new_novel";//エラー時は自画面遷移
 	        }
-		session.setAttribute("genre", novelSession.getGenre());
+		
+		 /*session.setAttribute("genre", novelSession.getGenre());
 		session.setAttribute("length", novelSession.getNovelLength());
 		session.setAttribute("title", novelSession.getTitle());
 		session.setAttribute("text", novelSession.getText());
 		//System.out.println(novelSession);
-		return "redirect:/novel/write";
+		 * */
+		 redirectAttributes.addFlashAttribute("session",novelSession);
+		 
+		return "redirect:/novel/write/save";
 	}
 	
 	@RequestMapping("novel/draft/list")
